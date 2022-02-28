76b23652b94ff497c81f618f7e2f7ab7068a65be
Pabloace86 <100536675+Pabloace86@users.noreply.github.com>
2022-02-28 10:21:18 -0300

    java- ejercicio 1 y ejercicio 2

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+
+        public static void main(String[] args) {
+	// ejercicio 1 crear variables
+            //tipoVariable-nameVariable-valorVarible
+            int numCats = 5;
+            double gradeExam = 13.2;
+            char letter = 'a';
+            String message = "hola mundo 123";
+            boolean isFree = false;
+
+            // ejercicio 2 - comentarios
+            /*
+            este ejrecicio
+            es un comentario
+            de varias lineas
+             */
+    }
+}
